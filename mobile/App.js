7a7a3f188c9b74fb21416046808df6f41013361f
Anjan Mahapatra <anjanmahapatra10@gmail.com>
2026-02-27 08:50:41 +0530

    Add Plan and History screens to navigation stack

--- mobile/App.js
+++ mobile/App.js
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Login from './app/login';
 import Home from './app/home';
+import PlanForm from './app/plan';
+import History from './app/history';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +14,8 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Plan" component={PlanForm} />
+        <Stack.Screen name="History" component={History} />
       </Stack.Navigator>
     </NavigationContainer>
   );
